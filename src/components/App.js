16c72a8c7e63358aacf80cb9c52fb1033796c86e
Grzegorz Szermiński <szerman@poczta.fm>
2019-06-06 12:28:37 +0200

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+// import logo from "./logo.svg";
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return <div className='app'>Aplikacja</div>;
+  }
+}
+
+export default App;
